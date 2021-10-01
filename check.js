4f60128aed16a8ca9683e5c9332c8d192b888e5c
Danko Aleksejevs <danko@very.lv>
2021-10-01 21:13:52 +0300

    Make checker work

---
+++ check.js
@@ -0,0 +1,7 @@
+result = true;
+
+setCheckerStatus(
+	'ok',
+	'No, it\'s working!',
+	'It looks like you are not affected by this particular problem. We still recommend to update your OS regularly!'
+);
